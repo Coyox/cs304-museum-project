6f19497f7295520e4d75ae5f2758570c6bec93a2
mila-engproj <mila.d.nikolic@gmail.com>
2015-03-25 16:01:36 -0700

    moving update member to own file to declutter and starting to connect methods to gui

---
+++ cs304-moa-project/src/Member.java
@@ -0,0 +1,39 @@
+
+import java.sql.*;
+// for reading from the command line
+import java.io.*;
+
+public class Member {
+	
+	public int updateMember(Connection con, String mname, String phone, 
+			String mname_new, String phone_new, String addr, int age, String email) {
+		PreparedStatement ps;
+		int count = -1;
+
+		String statement = "UPDATE member_1 SET mname =" + mname_new + 
+							", phone =" + phone_new +
+							", addr =" + addr +
+							", age =" + age +
+							", email =" + email +
+							"WHERE mname=" + mname +
+							"AND phone=" + phone;
+		
+		try {
+			ps = con.prepareStatement(statement);
+			
+			count = ps.executeUpdate();
+			con.commit();
+			ps.close();
+		} catch (SQLException ex) {
+			System.out.println("Message1: " + ex.getMessage());
+			try {
+				con.rollback();
+			} catch (SQLException ex2) {
+				System.out.println("Message2: " + ex2.getMessage());
+				System.exit(-1);
+			}
+		}
+
+		return count;
+	}
+}
